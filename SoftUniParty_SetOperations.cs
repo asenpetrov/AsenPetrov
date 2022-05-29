d8be476ca144031c380fac449f29df952ebf4a6f
Asen Petrov <106155007+asenpetrov@users.noreply.github.com>
2022-05-29 16:48:12 +0300

    Add files via upload

--- SoftUniParty_SetOperations.cs
+++ SoftUniParty_SetOperations.cs
@@ -8,6 +8,10 @@ namespace _08._SoftUni_Party
     {
         static void Main(string[] args)
         {
+            //There is a party.
+            //Many guests are invited and there are two types of them: VIP and Regular.
+            //When a guest comes, check if he/she exists in any of the two reservation lists.
+            
             HashSet<string> vipSet = new HashSet<string>();
             HashSet<string> regularSet = new HashSet<string>();
 
@@ -17,15 +21,16 @@ namespace _08._SoftUni_Party
                 //check if reservation is valid
                 if (ReservationCheck(currentReservation))
                 {
+                    //check if guest is VIP or Regular
                     if (ReservationType(currentReservation))
                     {
                         vipSet.Add(currentReservation);
                     }
+
                     else
                     {
                         regularSet.Add(currentReservation);
                     }
-
                 }
                 else
                 {
@@ -38,10 +43,14 @@ namespace _08._SoftUni_Party
             {
                 while (currentReservation != "END")
                 {
+                    //Removing the guests from the guestlists once they arrive
                     vipSet.Remove(currentReservation);
                     regularSet.Remove(currentReservation);
                     currentReservation = Console.ReadLine();
                 }
+
+                //Printing the count of the total guests that did not attend
+                //Printing the unique guests who did not attend
                 int sumTotal = vipSet.Count + regularSet.Count;
                 Console.WriteLine(sumTotal);
                 foreach (var vipGuest in vipSet)
