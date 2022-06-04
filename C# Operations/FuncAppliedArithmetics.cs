50c2fbab3186a73512423775905b87a44d8657f2
Asen Petrov <asenpetroff84@gmail.com>
2022-06-04 17:15:55 +0300

    Create FuncAppliedArithmetics.cs

---
+++ C# Operations/FuncAppliedArithmetics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //We read our numbers from the console.
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            //We prepare our functions for each operation
+            Func<List<int>, List<int>> add = list => list.Select(number => number += 1).ToList();
+            Func<List<int>, List<int>> multiply = list => list.Select(number => number *= 2).ToList();
+            Func<List<int>, List<int>> subtract = list => list.Select(number => number -= 1).ToList();
+
+            //We prepare the operation type
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = add(numbers);
+                }
+                else if (command == "multiply")
+                {
+                    numbers = multiply(numbers);
+                }
+                else if (command == "subtract")
+                {
+                    numbers = subtract(numbers);
+                }
+                else if (command == "print")
+                {
+                    Console.WriteLine(string.Join(" ", numbers));
+                }
+            }
+        }
+    }
+}
