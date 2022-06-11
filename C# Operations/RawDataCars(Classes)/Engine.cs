6f8061fb6b10503d1d112c604b42d34b22a0a9ec
Asen Petrov <asenpetroff84@gmail.com>
2022-06-11 17:29:38 +0300

    Create Engine.cs

---
+++ C# Operations/RawDataCars(Classes)/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    internal class Engine
+    {
+        //We create the Engine class with props and ctor.
+
+        public int Speed { get; set; }
+        public int Power { get; set; }
+
+        public Engine(int engineSpeed, int enginePower)
+        {
+            this.Speed = engineSpeed;
+            this.Power = enginePower;
+        }
+    }
+}
