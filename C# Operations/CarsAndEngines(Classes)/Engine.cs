787dc437cf2a31d10e86f041aef740d52eb53da3
Asen Petrov <asenpetroff84@gmail.com>
2022-06-11 17:14:12 +0300

    Create Engine.cs

---
+++ C# Operations/CarsAndEngines(Classes)/Engine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesMan
+{
+    internal class Engine
+    {
+        //We create a constructor for each possible input variation.
+        public Engine()
+        {
+
+        }
+        public Engine(string model, int power)
+        {
+            this.Model = model;
+            this.Power = power;
+        }
+        public Engine(string model, int power, int displacement)
+        {
+            this.Model = model;
+            this.Power = power;
+            this.Displacement = displacement;
+        }
+        public Engine(string model, int power, string efficiency)
+        {
+            this.Model = model;
+            this.Power = power;
+            this.Efficiency = efficiency;
+        }
+        public Engine(string model, int power, int displacement, string efficiency)
+        {
+            this.Model = model;
+            this.Power = power;
+            this.Efficiency = efficiency;
+            this.Displacement = displacement;
+
+        }
+        //We add the properties for each engine.
+        public string Model { get; set; }
+        public int Power { get; set; }
+        public int Displacement { get; set; }
+        public string Efficiency { get; set; }
+
+    }
+}
