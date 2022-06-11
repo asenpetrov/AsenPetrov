8ff422dfa5217942ef59b5d7658c62701e9e0134
Asen Petrov <asenpetroff84@gmail.com>
2022-06-11 17:29:35 +0300

    Create Cargo.cs

---
+++ C# Operations/RawDataCars(Classes)/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    internal class Cargo
+    {
+        //We create the Cargo class with props and ctor.
+        public string TypeCargo { get; set; }
+        public int WeightCargo { get; set; }
+
+        public Cargo(string type, int weight)
+        {
+            this.TypeCargo = type;
+            this.WeightCargo = weight;
+        }
+
+    }
+}
