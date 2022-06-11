1d1e6ce07f18a8664ea34fbff92d1b6a933b722a
Asen Petrov <asenpetroff84@gmail.com>
2022-06-11 17:29:43 +0300

    Create Tyres.cs

---
+++ C# Operations/RawDataCars(Classes)/Tyres.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawData
+{
+    internal class Tyres
+    {
+        //We create the Tyres class with props.
+
+        public double Pressure { get; set; }
+        public int TyreAge { get; set; }
+
+    }
+}
