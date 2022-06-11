4c1e263921bb7ff039dfb1a79bb7ddc7f06feb48
Asen Petrov <asenpetroff84@gmail.com>
2022-06-11 17:14:08 +0300

    Create Car.cs

---
+++ C# Operations/CarsAndEngines(Classes)/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesMan
+{
+    internal class Car
+    {
+        //We create a constructor for each possible input variation.
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+        public Car(string model, Engine engine, int weight)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = weight;
+        }
+        public Car(string model, Engine engine, string color)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Color = color;
+        }
+        public Car(string model, Engine engine, int weight, string color)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight= weight;
+            this.Color = color;
+        }
+        //We create the properties for each Car.
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        
+    }
+}
