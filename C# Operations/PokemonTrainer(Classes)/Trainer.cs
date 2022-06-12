aed05e34649fc0aa9624584ab5010fe72efe287b
Asen Petrov <asenpetroff84@gmail.com>
2022-06-12 18:43:34 +0300

    Create Trainer.cs

---
+++ C# Operations/PokemonTrainer(Classes)/Trainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon_Trainer
+{
+    public class Trainer
+    {
+        //We create the Trainer class with the respective props (+ the Pokemon class).
+
+        public Trainer(string name, int badges, List<Pokemon> pokemons)
+        {
+            this.Name = name;
+            this.Badges = badges;
+            this.Pokemons = pokemons;
+        }
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+    }
+}
