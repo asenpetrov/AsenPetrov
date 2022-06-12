b55a07ee75260f2f3deeb9b0d88033ad74c6bd4f
Asen Petrov <asenpetroff84@gmail.com>
2022-06-12 18:43:39 +0300

    Create Pokemon.cs

---
+++ C# Operations/PokemonTrainer(Classes)/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon_Trainer
+{
+    public class Pokemon
+    {
+        //We create the Pokemon class with the respective props.
+        public Pokemon(string name, string element, int health)
+        {
+            this.Name = name;
+            this.Element = element;
+            this.Health = health;
+        }
+        public string Name { get; set; }
+        public string Element { get; set; }
+        public int Health { get; set; }
+    }
+}
