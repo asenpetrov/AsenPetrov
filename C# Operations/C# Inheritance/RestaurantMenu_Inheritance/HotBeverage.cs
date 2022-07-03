df3f2396818e1bfe3139832255efe5750343bb97
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:08:36 +0300

    Create HotBeverage.cs

---
+++ C# Operations/C# Inheritance/RestaurantMenu_Inheritance/HotBeverage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    //HotBeverage is a child class to Beverage, but it has no additional properties.
+    public class HotBeverage : Beverage
+    {
+        public HotBeverage(string name, decimal price, double milliliters) 
+            : base(name, price, milliliters)
+        {
+
+        }
+    }
+}
