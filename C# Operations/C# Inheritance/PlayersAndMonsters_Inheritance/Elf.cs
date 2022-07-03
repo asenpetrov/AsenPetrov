6d98417ca99d1d5171ff406a061d915a3e150789
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:14:43 +0300

    Create Elf.cs

---
+++ C# Operations/C# Inheritance/PlayersAndMonsters_Inheritance/Elf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    //Elf is a child class to Hero and a Parent class to MuseElf
+
+    public class Elf : Hero
+    {
+        public Elf(string username, int level) : base(username, level)
+        {
+        }
+    }
+}
