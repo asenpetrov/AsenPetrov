d5680e176f6c324106acceb7f1f7c5fee66883c0
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:08:41 +0300

    Create Product.cs

---
+++ C# Operations/C# Inheritance/RestaurantMenu_Inheritance/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    //We create the base class Product with only 2 properties.
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
