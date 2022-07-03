0f0f5051de6aadf56ae9904156ff0bb68e20b1c5
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:08:22 +0300

    Create Cake.cs

---
+++ C# Operations/C# Inheritance/RestaurantMenu_Inheritance/Cake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    //The Cake has 3 Default values (Grams, Calories, Price) -> Calories is a property for the Desserts. 
+    public class Cake : Dessert
+    {
+        const double cakeGrams = 250;
+        const double cakeCalories = 1000;
+        const decimal cakePrice = 5;
+
+        public Cake(string name)
+            : base(name, cakePrice, cakeGrams, cakeCalories)
+        {
+
+        }
+    }
+}
