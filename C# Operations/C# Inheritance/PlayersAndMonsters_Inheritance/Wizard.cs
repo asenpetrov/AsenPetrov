75c7d1a5212704c1eeca4262edf3063a624afa79
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:14:58 +0300

    Create Wizard.cs

---
+++ C# Operations/C# Inheritance/PlayersAndMonsters_Inheritance/Wizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    //Wizard is a child class to Hero and a Parent class to DarkWizard.
+
+    public class Wizard : Hero
+    {
+        public Wizard(string username, int level) : base(username, level)
+        {
+        }
+    }
+}
