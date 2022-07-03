32da3e3a4a58df1165ed8559161fdcc5094a523e
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:08:23 +0300

    Create Coffee.cs

---
+++ C# Operations/C# Inheritance/RestaurantMenu_Inheritance/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    //Coffee is a child class to HotBeverage and it has 1 additional property (Caffeine)
+    //and 2 Default properties (Price and Milliliters).
+    public class Coffee : HotBeverage
+    {
+        const double CoffeeMilliliters = 50;
+        const decimal CoffeePrice = 3.50m;
+        public Coffee(string name, double caffeine)
+            : base(name, CoffeePrice, CoffeeMilliliters)
+        {
+            this.Caffeine = caffeine;
+        }
+      
+        public double Caffeine { get; set; }
+    }
+}
