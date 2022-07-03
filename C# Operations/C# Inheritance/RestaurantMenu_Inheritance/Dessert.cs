6a89308798ff96ee15d4b52b31adcb3584636f0c
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:08:27 +0300

    Create Dessert.cs

---
+++ C# Operations/C# Inheritance/RestaurantMenu_Inheritance/Dessert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    //The Dessert class has 1 additional property (Calories).
+    public class Dessert : Food
+    {
+        public Dessert(string name, decimal price, double grams, double calories) 
+            : base(name, price, grams)
+        {
+            this.Calories = calories;
+        }
+        public double Calories { get; set; }
+    }
+}
