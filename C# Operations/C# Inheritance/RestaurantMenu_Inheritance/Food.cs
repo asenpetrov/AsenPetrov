b09c31ab0a3df3a8f289659cf8e739b83a2e53f5
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:08:33 +0300

    Create Food.cs

---
+++ C# Operations/C# Inheritance/RestaurantMenu_Inheritance/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    //The Food class is a child to Product and has 1 additional property.
+    //The Food class has 3 child classes (Starter, MainDish, Dessert). 
+    public class Food : Product
+    {
+        public Food(string name, decimal price, double grams) 
+            : base(name, price)
+        {
+            this.Grams = grams;
+        }
+        public double Grams { get; set; }
+    }
+}
