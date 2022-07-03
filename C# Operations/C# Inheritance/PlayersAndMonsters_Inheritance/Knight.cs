fdb9d97bb7560df0e61aa81bd7706a3b65b4c9ef
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:14:49 +0300

    Create Knight.cs

---
+++ C# Operations/C# Inheritance/PlayersAndMonsters_Inheritance/Knight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    //Knight is a child class to Hero and a Parent class to DarkKnight
+    public class Knight : Hero
+    {
+        public Knight(string username, int level)
+            : base(username, level)
+        {
+        }
+    }
+}
