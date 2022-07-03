d1b92342c0c2053d438a58d8c3c7f8345fd14191
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 12:08:30 +0300

    Create Fish.cs

---
+++ C# Operations/C# Inheritance/RestaurantMenu_Inheritance/Fish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    //the Fish class has a Default value for the Grams.
+    public class Fish : MainDish
+    {
+        const double fishGrams = 22;
+        public Fish(string name, decimal price)
+            : base(name, price, fishGrams)
+        {
+        }
+    }
+}
