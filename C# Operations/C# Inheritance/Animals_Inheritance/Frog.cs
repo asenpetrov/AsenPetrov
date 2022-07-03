ce5298ea1f75d8f20da9d8aba74cc7dad1ae16e2
Asen Petrov <asenpetroff84@gmail.com>
2022-07-03 14:16:27 +0300

    Create Frog.cs

---
+++ C# Operations/C# Inheritance/Animals_Inheritance/Frog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Frog : Animal
+    {
+        public Frog(string name, int age, string gender) 
+            : base(name, age, gender)
+        {
+
+        }
+
+        //We override ProduceSound with the specific sound.
+        public override string ProduceSound()
+        {
+            return "Ribbit";
+        }
+    }
+}
