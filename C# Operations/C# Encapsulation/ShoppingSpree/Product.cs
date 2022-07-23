622b60e2bde86e34ec34228ffd1a92d9fb50aff7
Asen Petrov <asenpetroff84@gmail.com>
2022-07-23 21:49:27 +0300

    Create Product.cs

---
+++ C# Operations/C# Encapsulation/ShoppingSpree/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        //we create private fields
+        private string name;
+        private int price;
+
+        //we initiate our constructor with the name of the person and price
+        public Product(string name, int price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        //We make sure that the input is valid (name not null/empty and price not negative
+        public string Name 
+        { 
+            get 
+            {
+                return name;   
+            }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name cannot be empty");
+                }
+                name = value;
+            } 
+        }
+        public int Price 
+        {
+            get 
+            {
+                return price;
+            } 
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                price = value;
+            } 
+        }
+    }
+}
