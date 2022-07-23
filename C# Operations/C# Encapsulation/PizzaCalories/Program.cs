86bcbb8476346426c9481b43e4fb45602dfd0341
Asen Petrov <asenpetroff84@gmail.com>
2022-07-23 21:49:18 +0300

    Create Program.cs

---
+++ C# Operations/C# Encapsulation/PizzaCalories/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                //We receive the input from the console and create our Pizza, Dough and Topping/s
+                string pizzaName = Console.ReadLine().Split(" ")[1];
+                Pizza currentPizza = new Pizza (pizzaName);
+                double caloriesPizza = 0;
+
+                string[] doughInfo = Console.ReadLine().Split(" ");
+                string doughType = doughInfo[1];
+                string bakingTech = doughInfo[2];
+                int doughWeight = int.Parse(doughInfo[3]);
+                Dough currentDough = new Dough(doughType, bakingTech, doughWeight);
+
+                caloriesPizza += currentDough.CalculateCalories();
+                //currentPizza.TotalCalories += currentDough.TotalCalories(currentDough);
+
+                string toppingInfo = Console.ReadLine();
+                while (toppingInfo != "END")
+                {
+                    string[] token = toppingInfo.Split(" ");
+                    string toppingType = token[1];
+                    int toppingWeight = int.Parse(token[2]);
+
+                    Topping currentTopping = new Topping(toppingType, toppingWeight);
+                    currentPizza.Toppings.Add(currentTopping);
+                    caloriesPizza += currentTopping.CalculateCalories();
+
+                    toppingInfo = Console.ReadLine();
+                }
+                if (currentPizza.Toppings.Count>10)
+                {
+                    throw new ArgumentException("Number of toppings should be in range [0..10].");
+                }
+
+                Console.WriteLine($"{currentPizza.Name} - {caloriesPizza:f2} Calories.");
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }
+           
+        }
+    }
+}
