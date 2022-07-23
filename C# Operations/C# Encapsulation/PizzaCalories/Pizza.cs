b777e127fa96e4faeecde228ddb75d15b4ef8d40
Asen Petrov <asenpetroff84@gmail.com>
2022-07-23 21:49:10 +0300

    Create Pizza.cs

---
+++ C# Operations/C# Encapsulation/PizzaCalories/Pizza.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Pizza
+    {
+        private string name;
+        private List<Topping> toppings;
+        private int numberOfToppings;
+
+        public Pizza(string name)
+        {
+            this.Name = name;
+            this.Toppings = new List<Topping>();
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length > 15 || string.IsNullOrWhiteSpace(value) || string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Pizza name should be between 1 and 15 symbols.");
+                }
+                name = value;
+            }
+        }
+        public List<Topping> Toppings { get; set; }
+
+        public int NumberOfToppings
+        {
+            get
+            {
+                return numberOfToppings;
+            }
+            set
+            {
+                if (numberOfToppings < 0 || numberOfToppings > 10)
+                {
+                    throw new ArgumentException("Number of toppings should be in range [0..10].");
+                }
+            }
+        }
+     
+    }
+}
