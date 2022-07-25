a156042c2c829e1a43d625e2ace9e48a7dbc47e8
Asen Petrov <asenpetroff84@gmail.com>
2022-07-25 23:45:07 +0300

    Create IBirthdayable.cs

---
+++ C# Operations/C# Encapsulation/BirthdayCelebrations/IBirthdayable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    //We create IBirthdayable and implement it in some of the classes.
+    public interface IBirthdayable
+    {
+        public string BirthDate { get; set; }
+    }
+}
