b3570826533a312dbccb71dcd4813dc2562f35b4
Asen Petrov <asenpetroff84@gmail.com>
2022-07-25 23:45:28 +0300

    Create Citizen.cs

---
+++ C# Operations/C# Encapsulation/BorderControl/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentifier
+    {
+        //We create a class Citizen with appropriate properties which implements IIdentifier interface 
+        public Citizen(string name, int age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+    }
+}
