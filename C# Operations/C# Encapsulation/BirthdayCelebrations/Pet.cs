97eb3fd30f858d6d42bb0ea68c16c265421876a3
Asen Petrov <asenpetroff84@gmail.com>
2022-07-25 23:45:40 +0300

    Create Pet.cs

---
+++ C# Operations/C# Encapsulation/BirthdayCelebrations/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    public class Pet : IBirthdayable
+    {
+        public Pet(string name, string birthDate)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+        }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+    }
+}
