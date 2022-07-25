f3e14bfa815680094b232eb0f68727db19c3f33f
Asen Petrov <asenpetroff84@gmail.com>
2022-07-25 23:45:42 +0300

    Create IIdentifier.cs

---
+++ C# Operations/C# Encapsulation/BirthdayCelebrations/IIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    //We create IIdentifier and implement it in some of the classes.
+    public interface IIdentifier
+    {
+        public string Id { get; set; }
+    }
+}
