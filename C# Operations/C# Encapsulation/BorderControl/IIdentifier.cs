e709d9c7ea8552d579709fc156165924acfdb348
Asen Petrov <asenpetroff84@gmail.com>
2022-07-25 23:45:21 +0300

    Create IIdentifier.cs

---
+++ C# Operations/C# Encapsulation/BorderControl/IIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public interface IIdentifier
+    {
+        //We create IIdentifier and implement it in some of the classes.
+        public string Id { get; set; }
+    }
+}
