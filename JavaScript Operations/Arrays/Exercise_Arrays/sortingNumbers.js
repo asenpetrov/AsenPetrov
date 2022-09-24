0b9af4b416825d1be242cabfdce19f51a8f0c93d
Asen Petrov <asenpetroff84@gmail.com>
2022-09-24 21:57:43 +0300

    Update sortingNumbers.js

--- JavaScript Operations/Arrays/Exercise_Arrays/sortingNumbers.js
+++ JavaScript Operations/Arrays/Exercise_Arrays/sortingNumbers.js
@@ -2,25 +2,18 @@ function solve(array){
 
     let result = [];
 
+    array.sort((a, b) => a-b);
+
     
+    while(array.length > 0){
 
+        let currentMin = array.shift();
+        let currentMax = array.pop();
 
-    // for(var i = 0; i < array.length; i++){
+        result.push(currentMin);
+        result.push(currentMax);
+    }
 
-    //     let currentSmallest = Math.min(...array);
-    //     let currentBiggest = Math.max(...array);
-
-    //     let indexCurrentSmallest = array.indexOf(currentSmallest);
-    //     let indexCurrentBiggest = array.indexOf(currentBiggest);
-
-    //     result.push(currentSmallest);
-    //     result.push(currentBiggest);
-
-    //     arrays.splice(arrays.indexOf(currentSmallest), 1);
-    //     arrays.splice(arrays.indexOf(currentBiggest), 1);
-
-    // }
-
-    // return result;
+    return result;
 }
 solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
