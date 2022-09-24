95272f9c87479635b2d1b072abffd38d2a18040e
Asen Petrov <asenpetroff84@gmail.com>
2022-09-24 21:58:24 +0300

    Create fromJSONtoHTMLTable.js

---
+++ JavaScript Operations/ObjectsAndComposition/fromJSONtoHTMLTable.js
@@ -0,0 +1,44 @@
+function solve(json){
+
+    let parsed = JSON.parse(json);
+
+    let columnNames = Object.keys(parsed[0]);
+    let values = parsed.map(obj => Object.values(obj));
+    
+    let result = '<table>\n';
+    result += '   <tr>';
+
+    for(let columnName of columnNames){
+        result += `<th>${columnName}</th>`
+    }
+
+    result += '</tr>\n';
+
+    for(let value of values){
+
+        result += '   <tr>';
+        for (let i = 0; i < value.length; i++) {
+            
+            result += `<td>${value[i]}</td>`;
+        };
+        
+        //result += `<td>${value[0]}</td><td>${value[1]}</td>`
+        result += '</tr>\n';
+    }
+
+    result += '</table>\n';
+
+    console.log(result);
+}
+solve(
+    `[{"Name":"Pesho",
+    "Score":4,
+    " Grade":8},
+   {"Name":"Gosho",
+    "Score":5,
+    " Grade":8},
+   {"Name":"Angel",
+    "Score":5.50,
+    " Grade":10}]`
+
+)
