9282080de5fb332cf84001239eed9baba561b51f
Asen Petrov <asenpetroff84@gmail.com>
2022-09-25 22:02:27 +0300

    Update lowestPriceInCities.js

--- JavaScript Operations/ObjectsAndComposition/lowestPriceInCities.js
+++ JavaScript Operations/ObjectsAndComposition/lowestPriceInCities.js
@@ -1,37 +1,31 @@
-function solve(data){
+function solve(data) {
+  let result = {};
 
-    let result = {};
+  for (let el of data) {
+    let [town, product, price] = el.split(" | ");
+    price = Number(price);
 
-    for(let el of data){
-        
-        let [town, product, price] = el.split(" | ");
-        price = Number(price);
+    if (result.hasOwnProperty(product)) {
+      let currentPrice = result[product]["price"];
 
-        if(result.hasOwnProperty(product)){
-            
-
-        
-        } else if(!result[product].hasOwnProperty(town)){
-            result[product][town] = price;
-        } else if(result[product][town] > price){
-            result[product][town] = price;
-        }
-
+      if (currentPrice > price) {
+        result[product] = { town, price };
+      }
+    } else {
+      result[product] = { town, price };
     }
+  }
 
-    for(let [town, value] of Object.entries(result)){
-        for(let [product, price] of Object.entries(value)){
-            console.log(`${product} -> ${price} ${(town)}`);    
-        }
-    }
-
+  for (let [product, value] of Object.entries(result)) {
+    console.log(`${product} -> ${value.price} (${value.town})`);
+  }
 }
-solve(
-['Sample Town | Sample Product | 1000',
-'Sample Town | Orange | 2',
-'Sample Town | Peach | 1',
-'Sofia | Orange | 3',
-'Sofia | Peach | 2',
-'New York | Sample Product | 1000.1',
-'New York | Burger | 10']
-);
+solve([
+  "Sample Town | Sample Product | 1000",
+  "Sample Town | Orange | 2",
+  "Sample Town | Peach | 1",
+  "Sofia | Orange | 3",
+  "Sofia | Peach | 2",
+  "New York | Sample Product | 1000.1",
+  "New York | Burger | 10",
+]);
