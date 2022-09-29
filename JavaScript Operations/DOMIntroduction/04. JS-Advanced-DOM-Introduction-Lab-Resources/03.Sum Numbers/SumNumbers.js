2195c153fc55d3d80458a29822892b051b9db683
Asen Petrov <asenpetroff84@gmail.com>
2022-09-29 21:56:09 +0300

    Create SumNumbers.js

---
+++ JavaScript Operations/DOMIntroduction/04. JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,9 @@
+function calc() {
+
+    let firstNum = document.getElementById("num1").value;
+    let secondNum = document.getElementById("num2").value;
+    let sum = document.getElementById("sum");
+
+    sum.value = Number(firstNum) + Number(secondNum);
+
+}
