c4677b1fee53b3314e22dc9bef079b27b6077489
Asen Petrov <asenpetroff84@gmail.com>
2022-09-29 21:56:13 +0300

    Create ShowMore.js

---
+++ JavaScript Operations/DOMIntroduction/04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+ 
+    let textToShow = document.getElementById("text");
+    let link = document.getElementById("more");
+
+    textToShow.style.display = "inline";
+    link.style.display = "none";
+}
